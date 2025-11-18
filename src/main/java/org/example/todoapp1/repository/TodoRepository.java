e9d9cfa770551cf4c8231547dd1ed7c3df532385
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-18 15:07:50 +0900

    create 로직 작성

--- src/main/java/org/example/todoapp1/repository/TodoRepository.java
+++ src/main/java/org/example/todoapp1/repository/TodoRepository.java
@@ -9,4 +9,10 @@ public class TodoRepository {
     // storage 라는 이름의 Map( 키 와 값)을 저장
     private final Map<Long, TodoDto> storage = new ConcurrentHashMap<>();
     private Long nextId = 1L;
+
+    public TodoDto save(TodoDto todo) {
+        todo.setId(nextId++);     // ① ID 자동 증가하여 부여
+        storage.put(todo.getId(), todo);  // ② Map 저장소에 저장
+        return todo;              // ③ 저장된 TodoDto 다시 반환
+    }
 }
