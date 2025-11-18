13cc9673e5550e91891a08275ae345c2b1a1e542
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-18 14:30:10 +0900

    기본세팅

---
+++ src/test/java/org/example/todoapp1/Todoapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todoapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todoapp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
